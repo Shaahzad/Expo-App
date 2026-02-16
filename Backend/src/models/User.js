6aaa53a575b758efc3cfe4445dc76e739633a76b
Shaahzad <shehzadmemon147@gmail.com>
2026-02-16 23:38:34 +0500

    Complete register & login route

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -23,6 +23,16 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.pre("save", async function(next){
+if(!this.Modified("password")) return next()
+const salt = await bcrypt.genSalt(10)
+this.password = await bcrypt.hash(this.password, salt)
+next()
+})
+
+userSchema.methods.comparePassword = async function (userPassword) {
+return await bcrypt.compare(userPassword, this.password)
+}
 
 const User = mongoose.model("User", userSchema)
 
